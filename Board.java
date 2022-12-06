e6f0a6e8def8ef753118eb798364901103659712
Howell Jenkins <s4pideam@uni-trier.de>
2022-12-06 01:06:55 +0100

    Simplified drawBoard
    Extracted g.fillRect(x,y,w,h); updateMap(x,y,w,h); into a Function for better Reading

--- Board.java
+++ Board.java
@@ -233,117 +233,92 @@ public class Board extends JPanel
     g.drawString("Clear High Scores",180,max+5+gridSize);
     g.drawString("Exit",350,max+5+gridSize);
   }
-  
-  
+
+
+
+  private void fillAndUpdateMap(Graphics g , int x, int y, int w, int h) {
+    g.fillRect(x,y,w,h);
+    updateMap(x,y,w,h);
+  }
   /*  This function draws the board.  The pacman board is really complicated and can only feasibly be done
       manually.  Whenever I draw a wall, I call updateMap to invalidate those coordinates.  This way the pacman
       and ghosts know that they can't traverse this area */ 
   public void drawBoard(Graphics g)
   {
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,600,600);
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,420,420);
-        
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,20,600);
-        g.fillRect(0,0,600,20);
-        g.setColor(Color.WHITE);
-        g.drawRect(19,19,382,382);
-        g.setColor(Color.BLUE);
+    g.setColor(Color.BLACK);
+    g.fillRect(0,0,600,600);
+    g.setColor(Color.BLACK);
+    g.fillRect(0,0,420,420);
 
-        g.fillRect(40,40,60,20);
-          updateMap(40,40,60,20);
-        g.fillRect(120,40,60,20);
-          updateMap(120,40,60,20);
-        g.fillRect(200,20,20,40);
-          updateMap(200,20,20,40);
-        g.fillRect(240,40,60,20);
-          updateMap(240,40,60,20);
-        g.fillRect(320,40,60,20);
-          updateMap(320,40,60,20);
-        g.fillRect(40,80,60,20);
-          updateMap(40,80,60,20);
-        g.fillRect(160,80,100,20);
-          updateMap(160,80,100,20);
-        g.fillRect(200,80,20,60);
-          updateMap(200,80,20,60);
-        g.fillRect(320,80,60,20);
-          updateMap(320,80,60,20);
+    g.setColor(Color.BLACK);
+    g.fillRect(0,0,20,600);
+    g.fillRect(0,0,600,20);
+    g.setColor(Color.WHITE);
+    g.drawRect(19,19,382,382);
+    g.setColor(Color.BLUE);
 
-        g.fillRect(20,120,80,60);
-          updateMap(20,120,80,60);
-        g.fillRect(320,120,80,60);
-          updateMap(320,120,80,60);
-        g.fillRect(20,200,80,60);
-          updateMap(20,200,80,60);
-        g.fillRect(320,200,80,60);
-          updateMap(320,200,80,60);
-
-        g.fillRect(160,160,40,20);
-          updateMap(160,160,40,20);
-        g.fillRect(220,160,40,20);
-          updateMap(220,160,40,20);
-        g.fillRect(160,180,20,20);
-          updateMap(160,180,20,20);
-        g.fillRect(160,200,100,20);
-          updateMap(160,200,100,20);
-        g.fillRect(240,180,20,20);
-        updateMap(240,180,20,20);
-        g.setColor(Color.BLUE);
+    fillAndUpdateMap(g,40,40,60,20);
+    fillAndUpdateMap(g,120,40,60,20);
+    fillAndUpdateMap(g,200,20,20,40);
+    fillAndUpdateMap(g,240,40,60,20);
+    fillAndUpdateMap(g,320,40,60,20);
+    fillAndUpdateMap(g,40,80,60,20);
+    fillAndUpdateMap(g,160,80,100,20);
+    fillAndUpdateMap(g,200,80,20,60);
+    fillAndUpdateMap(g,320,80,60,20);
 
 
-        g.fillRect(120,120,60,20);
-          updateMap(120,120,60,20);
-        g.fillRect(120,80,20,100);
-          updateMap(120,80,20,100);
-        g.fillRect(280,80,20,100);
-          updateMap(280,80,20,100);
-        g.fillRect(240,120,60,20);
-          updateMap(240,120,60,20);
+    fillAndUpdateMap(g,20,120,80,60);
+    fillAndUpdateMap(g,320,120,80,60);
+    fillAndUpdateMap(g,20,200,80,60);
+    fillAndUpdateMap(g,320,200,80,60);
 
-        g.fillRect(280,200,20,60);
-          updateMap(280,200,20,60);
-        g.fillRect(120,200,20,60);
-          updateMap(120,200,20,60);
-        g.fillRect(160,240,100,20);
-          updateMap(160,240,100,20);
-        g.fillRect(200,260,20,40);
-          updateMap(200,260,20,40);
 
-        g.fillRect(120,280,60,20);
-          updateMap(120,280,60,20);
-        g.fillRect(240,280,60,20);
-          updateMap(240,280,60,20);
+    fillAndUpdateMap(g,160,160,40,20);
+    fillAndUpdateMap(g,220,160,40,20);
+    fillAndUpdateMap(g,160,180,20,20);
+    fillAndUpdateMap(g,160,200,100,20);
+    fillAndUpdateMap(g,240,180,20,20);
 
-        g.fillRect(40,280,60,20);
-          updateMap(40,280,60,20);
-        g.fillRect(80,280,20,60);
-          updateMap(80,280,20,60);
-        g.fillRect(320,280,60,20);
-          updateMap(320,280,60,20);
-        g.fillRect(320,280,20,60);
-          updateMap(320,280,20,60);
+    g.setColor(Color.BLUE);
 
-        g.fillRect(20,320,40,20);
-          updateMap(20,320,40,20);
-        g.fillRect(360,320,40,20);
-          updateMap(360,320,40,20);
-        g.fillRect(160,320,100,20);
-          updateMap(160,320,100,20);
-        g.fillRect(200,320,20,60);
-          updateMap(200,320,20,60);
 
-        g.fillRect(40,360,140,20);
-          updateMap(40,360,140,20);
-        g.fillRect(240,360,140,20);
-          updateMap(240,360,140,20);
-        g.fillRect(280,320,20,40);
-          updateMap(280,320,20,60);
-        g.fillRect(120,320,20,60);
-          updateMap(120,320,20,60);
-        drawLives(g);
-  } 
+    fillAndUpdateMap(g,120,120,60,20);
+    fillAndUpdateMap(g,120,80,20,100);
+    fillAndUpdateMap(g,280,80,20,100);
+    fillAndUpdateMap(g,240,120,60,20);
+
+
+    fillAndUpdateMap(g,280,200,20,60);
+    fillAndUpdateMap(g,120,200,20,60);
+    fillAndUpdateMap(g,160,240,100,20);
+    fillAndUpdateMap(g,200,260,20,40);
+
+
+    fillAndUpdateMap(g,120,280,60,20);
+    fillAndUpdateMap(g,240,280,60,20);
+
+
+    fillAndUpdateMap(g,40,280,60,20);
+    fillAndUpdateMap(g,80,280,20,60);
+    fillAndUpdateMap(g,320,280,60,20);
+    fillAndUpdateMap(g,320,280,20,60);
+
+
+    fillAndUpdateMap(g,20,320,40,20);
+    fillAndUpdateMap(g,360,320,40,20);
+    fillAndUpdateMap(g,160,320,100,20);
+    fillAndUpdateMap(g,200,320,20,60);
+
+
+    fillAndUpdateMap(g,40,360,140,20);
+    fillAndUpdateMap(g,240,360,140,20);
+    fillAndUpdateMap(g,280,320,20,40);
+    fillAndUpdateMap(g,120,320,20,60);
+
+    drawLives(g);
+  }
+
 
 
   /* Draws the pellets on the screen */
